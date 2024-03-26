3bdb4fee50f21340f566a4822a540b190faedeeb
teoMarinov <teodor.b.marinov@gmail.com>
2024-03-26 17:36:54 +0200

    create sidebae

--- app/(dashboard)/_components/sidebar/index.tsx
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -1,12 +1,15 @@
 "use client";
 
+import { List } from "./list";
+import { NewButton } from "./new-button";
+
 interface SidebarProps {}
 
 const Sidebar: React.FC<SidebarProps> = ({}) => {
   return (
     <aside className="fixed z-[1] left-0 bg-blue-950 h-full w-[60px] flex p-3 flex-col gap-y-4 text-white">
-      <div></div>
-      Side
+      <List />
+      <NewButton />
     </aside>
   );
 };
---
+++ app/(dashboard)/_components/sidebar/new-button.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { Plus } from "lucide-react";
+
+import React from "react";
+
+import { CreateOrganization } from "@clerk/nextjs";
+
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+
+export const NewButton = () => {
+  return (
+    <Dialog>
+      <DialogTrigger>
+        <div className="aspect-square">
+          <button className="bg-white/25 h-full w-full rounded-md flex items-center justify-center opacity-60 hover:opacity-100 transition">
+            <Plus className="text-white" />
+          </button>
+        </div>
+      </DialogTrigger>
+      <DialogContent className="p-0 bg-transparent border-none min-w-[480px]">
+        <CreateOrganization />
+      </DialogContent>
+    </Dialog>
+  );
+};
