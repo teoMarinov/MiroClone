3e2a2e0490e501177e9f13eec5df529b90390c56
teoMarinov <teodor.b.marinov@gmail.com>
2024-03-27 14:21:39 +0200

    add searchbar to navbar

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import { UserButton } from "@clerk/nextjs";
+import { SearchInput } from "./search-input";
 
 interface NavbarProps {}
 const Navbar: React.FC<NavbarProps> = ({}) => {
   return (
-    <div className="flex items-center gap-x-4 p-5 bg-green-500">
-      <div className="hidden lg:flex-1 lg:flex bg-yellow-500">
-        Search{/* TODO : Add Search */}
+    <div className="flex items-center gap-x-4 p-5">
+      <div className="hidden lg:flex-1 lg:flex">
+        <SearchInput />
       </div>
       <UserButton />
     </div>
