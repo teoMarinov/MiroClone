4279cedd7195ce1fb89ac43743ac3b39c2c80e2f
teoMarinov <teodor.b.marinov@gmail.com>
2024-03-28 17:52:43 +0200

    add dropdown trigger to board cards

--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -7,6 +7,8 @@ import { Overlay } from "./overlay";
 import { useAuth } from "@clerk/nextjs";
 import { Footer } from "./footer";
 import { Skeleton } from "@/components/ui/skeleton";
+import { Actions } from "@/components/actions";
+import { MoreHorizontal } from "lucide-react";
 
 interface BoardCardProps {
   id: string;
@@ -40,8 +42,13 @@ export const BoardCard = ({
     <Link href={`/board/${id}`}>
       <div className="group aspect-[100/127] border rounded-lg flex flex-col justify-between overflow-hidden">
         <div className="relative flex-1 bg-amber-50">
-          <Image src={imageUrl} alt={title} fill className="object-fill" />
+          <Image src={imageUrl} alt={title} fill className="object-fit" />
           <Overlay />
+          <Actions id={id} title={title} side="right">
+            <button className="absolute top-1 right-1 opacity-0 group-hover:opacity-100 transition-opacity px-3 py-2 outline-none">
+              <MoreHorizontal className="text-white opacity-75 hover:opacity-100 transition-opacity" />
+            </button>
+          </Actions>
         </div>
         <Footer
           isFavorite={isFavorite}
@@ -59,7 +66,7 @@ export const BoardCard = ({
 BoardCard.Skeleton = function BoardCardSkeleton() {
   return (
     <div className="aspect-[100/127] rounded-lg overflow-hidden">
-      <Skeleton className="size-full" /> 
+      <Skeleton className="h-full w-full" />
     </div>
-  )
-}
+  );
+};
--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -15,7 +15,7 @@ const DashboardPage = ({ searchParams }: DashboardPageProps) => {
   const { organization } = useOrganization();
 
   return (
-    <div className=" flex-1 h-[calc(100%-80px)] p-6">
+    <div className="flex-1 h-[calc(100%-80px)] p-6">
       {!organization ? <EmptyOrg /> : <BoardList orgId={organization.id} query={searchParams}/>}
     </div>
   );
