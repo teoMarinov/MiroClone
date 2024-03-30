e393050e53219f3d2b34732393ff9788493b859b
teoMarinov <teodor.b.marinov@gmail.com>
2024-03-30 13:13:54 +0200

    create add to favorites functionality

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,5 +1,6 @@
 import { v } from "convex/values";
 import { defineSchema, defineTable } from "convex/server";
+import { useDeferredValue } from "react";
 
 export default defineSchema({
   boards: defineTable({
@@ -14,4 +15,13 @@ export default defineSchema({
       searchField: "title",
       filterFields: ["orgId"],
     }),
+  userFavorites: defineTable({
+    orgId: v.string(),
+    userId: v.string(),
+    boardId: v.id("boards"),
+  })
+    .index("by_board", ["boardId"])
+    .index("by_user_org", ["userId", "orgId"])
+    .index("by_user_board", ["userId", "boardId"])
+    .index("by_user_board_org", ["userId", "boardId", "orgId"]),
 });
